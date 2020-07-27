23bb76ffe4a4b5f098166560c60ac7057a724b4d
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2020-07-27 23:23:46 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('time').innerHTML =
+    h + ":" + m ;
+    document.getElementById('time2').innerHTML=
+    h + ":" + m ;
+    document.getElementById('time3').innerHTML=
+    h + ":" + m ;
+    var t = setTimeout(startTime, 500);
+
+    
+    
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
